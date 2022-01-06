12b833adb0724c597c7e118071aaf14667761136
KingCobra29A <brunerk@umich.edu>
2022-01-05 20:09:48 -0500

    Folder structure, workspace, computer selection function

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function computerPlay(){
+    const ResultDict = {
+        '0': 'Rock',
+        '1': 'Paper',
+        '2': 'Scissors'
+    };
+    return ResultDict[Math.floor(Math.random() * 3)];    
+}
+
+function playRound(){
+
+}
+
+function game(){
+    
+}
